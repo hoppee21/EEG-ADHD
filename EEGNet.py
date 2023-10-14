8c27d5db53aac29ab73dde7daa8c41e1f46cb89b
Yihui <81303624+hoppee21@users.noreply.github.com>
2023-10-14 03:31:51 -0600

    Update EEGNet.py

--- EEGNet.py
+++ EEGNet.py
@@ -1,3 +1,4 @@
+import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
@@ -28,6 +29,8 @@ class EEGNet_encoder(pl.LightningModule):
         self.avgpool3 = nn.AvgPool2d((4, 8))
 
         self.flatten = nn.Flatten()
+
+        self.loss_function = MaxMarginContrastiveLoss(margin=(1.0, 3.0))
     
     def forward(self, x):
         x = self.conv1(x)
@@ -56,9 +59,50 @@ class EEGNet_encoder(pl.LightningModule):
     def training_step(self, batch, batch_idx):
         x, y = batch
         y_hat = self(x)
-        loss = F.mse_loss(y_hat, y)
+        loss = self.loss_function(y_hat, y)
+
+        # Log the loss for visualization in TensorBoard
+        self.log('train_loss', loss, on_step=True, on_epoch=True, prog_bar=True, logger=True)
+        
         return loss
 
     def configure_optimizers(self):
         optimizer = optim.Adam(self.parameters(), lr=1e-3)
         return optimizer
+    
+
+class MaxMarginContrastiveLoss(nn.Module):
+    def __init__(self, margin):
+        super(MaxMarginContrastiveLoss, self).__init__()
+        self.margin_id = margin[0]
+        self.margin_type = margin[1]
+
+    def forward(self, y, z):
+        labels_id = y[:, 0]
+        labels_type = y[:, 1]
+
+        # Compute pair-wise distance matrix
+        D = pdist_euclidean(z)
+        d_vec = D.view(-1, 1)
+
+        # Make contrastive labels
+        yid_contrasts = get_contrast_batch_labels(labels_id)
+        ytype_contrasts = get_contrast_batch_labels(labels_type)
+
+        loss = F.margin_ranking_loss(d_vec, yid_contrasts, margin=self.margin_id) + \
+               F.margin_ranking_loss(d_vec, ytype_contrasts, margin=self.margin_type)
+
+        return torch.mean(loss)
+
+def pdist_euclidean(A):
+    r = torch.sum(A*A, dim=1)
+    r = r.view(-1, 1)
+    D = r - 2*torch.matmul(A, torch.transpose(A, 0, 1)) + torch.transpose(r, 0, 1)
+    return torch.sqrt(D)
+
+def get_contrast_batch_labels(y):
+    y_col_vec = y.view(-1, 1).float()
+    D_y = pdist_euclidean(y_col_vec)
+    d_y = D_y.view(-1, 1)
+    y_contrasts = (d_y == 0).int()
+    return y_contrasts
